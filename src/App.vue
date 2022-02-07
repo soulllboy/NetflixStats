7dfa3ca1599bb0c0dfd5635dbdf088eaebef6a35
soulboy <soulllboy1@gmail.com>
2022-02-07 23:13:44 +0500

    practice

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> -->
   <router-view />
 </template>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/CardPiece.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="outer">
+    <img :src="movie.poster" />
+    <p>{{ movie }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardPiece",
+  props: {
+    movie: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+div.outer {
+  width: 536px;
+  height: 264px;
+  display: flex;
+  align-items: center;
+  padding: 24px;
+
+  background: white;
+  border: 1px solid rgba(42, 49, 53, 0.08);
+  box-shadow: 0px 2px 8px rgba(42, 49, 53, 0.16);
+  border-radius: 24px;
+}
+p {
+  color: red;
+}
+</style>
